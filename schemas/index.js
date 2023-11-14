e3b0ba4004ba6d3e0a23f7be9ca0e3f593e8f0be
lovekmg <rlaaudruakr@gmail.com>
2023-11-14 13:03:31 +0900

    Change mongodb connection info localhost -> 127.0.0.1

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = () => {
     mongoose
-        .connect("mongodb://localhost:27017/spa_mall")
+        .connect("mongodb://127.0.0.1:27017/spa_mall")
         .then(() => console.log("Connected on MongoDB!!"))
         .catch((err) => console.log(err));
 };
